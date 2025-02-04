d0b4bd4103c1fb5dc96b80aecb8517963e79dab3
Mohammad <mohammad.kh1742000@gmail.com>
2025-02-04 10:04:02 +0300

    Delete a line

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace GitPractice
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World !");
-            Console.WriteLine(" How are you !");
+            
             Class1.PrintName();
         }
     }
