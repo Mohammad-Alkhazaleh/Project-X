6d733f464c0b7c1830082169b6eac3fe2903e501
Mohammad <mohammad.kh1742000@gmail.com>
2025-02-04 09:08:52 +0300

    add new line.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace GitPractice
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World !");
+            Console.WriteLine(" How are you !");
         }
     }
 }
